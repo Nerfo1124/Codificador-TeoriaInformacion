34b9f128baf1928c3c4b6248141bf6102e9865c9
Nerfo1124 <dfernando1124@hotmail.com>
2018-03-14 15:38:32 -0500

    Actualizacion de fuentes, codigo @Milena. Funcionalidad al 80%

--- src/main/java/co/com/udistrital/teoria/pruebas/TestMachine.java
+++ src/main/java/co/com/udistrital/teoria/pruebas/TestMachine.java
@@ -7,6 +7,8 @@ package co.com.udistrital.teoria.pruebas;
 
 import co.com.udistrital.teoria.vo.MachineStatesVO;
 import co.com.udistrital.teoria.vo.StatesVO;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,7 +17,103 @@ import java.util.Scanner;
  */
 public class TestMachine {
 
-    public static void main(String[] args) {
+    public String ecuacion1, ecuacion2, ecuacion3;
+    Integer numMemories;
+    MachineStatesVO machine;
+
+    public void RecibeCircuitos(String c1, String c2, String c3) {
+
+        this.ecuacion1 = c1;
+        this.ecuacion2 = c2;
+        this.ecuacion3 = c3;
+
+    }
+
+    public void recibeMemorias(int n) {
+        //System.out.println("Ingrese numero de elementos de memoria: ");
+        numMemories = n;
+    }
+
+    public String execute() {
+        String response = "";
+        String responseHead = "E\t";
+        String responseBody = "";
+        Scanner sc = new Scanner(System.in);
+
+        Integer numEstados = (int) Math.pow(2, numMemories);
+        System.out.println("Numero de estados: " + numEstados);
+
+        machine = new MachineStatesVO();
+        if (this.ecuacion1 != null) {
+            machine.addOperacion(this.ecuacion1);
+        }
+        if (this.ecuacion2 != null) {
+            machine.addOperacion(this.ecuacion2);
+        }
+        if (this.ecuacion3 != null) {
+            machine.addOperacion(this.ecuacion3);
+        }
+        for (int i = 0; i < numEstados; i++) {
+            if ((numMemories - Integer.toBinaryString(i).length()) > 0) {
+                String regex = "%0" + (numMemories - Integer.toBinaryString(i).length()) + "d";
+//                System.out.println("Prueba: " + String.format(regex, 0) + Integer.toBinaryString(i));
+                machine.addEstado(new StatesVO("S" + i, String.format(regex, 0) + Integer.toBinaryString(i)));
+            } else {
+//                System.out.println("Prueba: " + Integer.toBinaryString(i));
+                machine.addEstado(new StatesVO("S" + i, Integer.toBinaryString(i)));
+            }
+        }
+
+//        Integer cont = 0;
+//        while(cont < 3) {
+//            System.out.println("Ingrese la ecuacion de su "+(cont+1)+" ecuacion:");
+//        }
+        for (int i = 0; i < numMemories; i++) {
+            responseHead += "M" + (i + 1) + "\t";
+        }
+        for (int i = 0; i < numMemories; i++) {
+            responseHead += "M" + (i + 1) + "+\t";
+        }
+        for (int i = 0; i < machine.getOperaciones().size(); i++) {
+            responseHead += "O" + (i + 1) + "\t";
+        }
+        responseHead += "S" + "\t";
+        responseHead += "S+" + "\t";
+
+        Integer[] entrada = {0, 1};
+        for (StatesVO estado : machine.getEstados()) {
+            for (Integer in : entrada) {
+                responseBody += in + "\t";
+                char[] auxC1 = estado.valuesToString().toCharArray();
+                for (char c : auxC1) {
+                    responseBody += c + "\t";
+                }
+                char[] auxC2 = estado.nextValues(in).toCharArray();
+                for (char c : auxC2) {
+                    responseBody += c + "\t";
+                }
+                for (Integer salida : estado.getSalidas(in)) {
+                    responseBody += salida + "\t";
+                }
+                responseBody += estado.getName() + "\t";
+                for (StatesVO est : machine.getEstados()) {
+                    if (est.valuesToString().equals(estado.nextValues(in))) {
+                        responseBody += est.getName() + "\t";
+                        break;
+                    }
+                }
+                responseBody += "\n";
+            }
+        }
+        System.out.println(responseHead);
+        System.out.println(responseBody);
+        response += responseHead + "\n";
+        response += responseBody;
+        return response;
+    }
+
+    /*  public static void main(String[] args) {
+     /*   
         String responseHead = "E\t";
         String responseBody = "";
         Scanner sc = new Scanner(System.in);
@@ -23,9 +121,7 @@ public class TestMachine {
         System.out.println("Ingrese numero de elementos de memoria: ");
         Integer numMemories = sc.nextInt();
 
-        String ecuacion1 = "E";
-        String ecuacion2 = "E XOR M1";
-        String ecuacion3 = "M1 XOR M2";
+        
 
 //        System.out.println("Ingrese la ecuacion del sistema, si desea termina digite 0:");
 //        String ecuacion = sc.nextLine();
@@ -44,7 +140,7 @@ public class TestMachine {
         System.out.println("Numero de estados: " + numEstados);
 
         MachineStatesVO machine = new MachineStatesVO();
-        machine.addOperacion(ecuacion1);
+        machine.addOperacion(this.ecuacion1);
         machine.addOperacion(ecuacion2);
         machine.addOperacion(ecuacion3);
         for (int i = 0; i < numEstados; i++) {
@@ -126,8 +222,7 @@ public class TestMachine {
         }
         System.out.println(printEstados);
         System.out.println("Palabra codificada: " + codificacion);
-    }
-
+    }*/
     public static String wordToBinary(String word) {
         String response = "";
         char[] array = word.toCharArray();
@@ -138,6 +233,36 @@ public class TestMachine {
         }
 //        System.out.println("Palabra original: " + word);
 //        System.out.println("Palabra en Binario: " + response);
+        return response;
+    }
+
+    public List<String> codificarPalabra(String palabra) {
+        List<String> response = new ArrayList<>();
+        String palabraBinario = wordToBinary(palabra);
+        System.out.println("Palabra en Binario: " + palabraBinario);
+
+        response.add(palabraBinario);
+        // Creacion de un array con los bits a codificar
+        char[] bin = palabraBinario.toCharArray();
+        // Codificacion de la palabra
+        String codificacion = "";
+        String printEstados = "";
+        String estInicial = String.format("%0" + (numMemories) + "d", 0);
+        for (char c : bin) {
+            Integer input = Integer.parseInt("" + c);
+            for (StatesVO estado : machine.getEstados()) {
+                if (estado.valuesToString().equals(estInicial)) {
+                    printEstados += estado.getName() + ",";
+                    codificacion += estado.salidasToString(input);
+                    estInicial = estado.nextValues(input);
+                    break;
+                }
+            }
+        }
+        System.out.println(printEstados);
+        response.add(printEstados);
+        System.out.println("Palabra codificada: " + codificacion);
+        response.add(codificacion);
         return response;
     }
 }
